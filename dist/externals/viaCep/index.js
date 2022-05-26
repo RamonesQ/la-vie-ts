5c6458e38a54e0d9d8835f1981314c50a32b7df2
RamonoesQ <ramonqueirozs@gmail.com>
2022-05-26 18:19:03 -0300

    Initial commit

---
+++ dist/externals/viaCep/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ViaCepApi = void 0;
+const ViaCep_1 = require("./ViaCep");
+exports.ViaCepApi = new ViaCep_1.ViaCep();
+// console.log(ViaCepApi.getAddress('01001000'));
---
+++ src/externals/viaCep/index.ts
@@ -0,0 +1,5 @@
+import { ViaCep } from "./ViaCep";
+
+export const ViaCepApi = new ViaCep();
+
+// console.log(ViaCepApi.getAddress('01001000'));
